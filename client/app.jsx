ee0637dbafd378a19250521ed84fd745c44ab434
Rob Currie <rcurrie@ucsc.edu>
2017-06-29 04:52:51 +0000

    Initial searchkit hacking

--- client/app.jsx
+++ client/app.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    SearchkitManager,
+    SearchkitProvider,
+    SearchBox,
+    Hits,
+} from 'searchkit';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {},
-    };
-  }
-  render() {
+const sk = new SearchkitManager('/es');
+
+class MovieHits extends Hits {
+  renderResult(result) {
     return (
-      <div>Dude</div>
+      <div key={result._id}>
+        <a target="_blank">
+          <div>{result._source.title}</div>
+        </a>
+      </div>
     );
   }
+}
+
+function App() {
+  return (
+    <div className="search-site">
+      <SearchkitProvider searchkit={sk}>
+        <div>
+          <div className="search-site__query">
+            <SearchBox />
+          </div>
+
+          <div className="search-site__results">
+            <MovieHits hitsPerPage={10} />
+          </div>
+        </div>
+      </SearchkitProvider>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
